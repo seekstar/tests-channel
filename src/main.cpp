eaae9a437011309fc7674065a438e44bf7638af8
Jiansheng Qiu <jianshengqiu.cs@gmail.com>
2023-09-24 23:34:03 +0800

    Remove some newlines to make code more compact

--- src/main.cpp
+++ src/main.cpp
@@ -40,23 +40,18 @@ TEST(SPSC, RepetitiveSendRecvIntSize1Num1) {
 TEST(SPSC, RepetitiveSendRecvIntSize1Num1e5) {
 	spsc_repetitive_send_recv(1, 233, 100000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize2Num1e5) {
 	spsc_repetitive_send_recv(2, 233, 100000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize4Num1e5) {
 	spsc_repetitive_send_recv(4, 233, 100000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize8Num1e5) {
 	spsc_repetitive_send_recv(8, 233, 100000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize16Num1e5) {
 	spsc_repetitive_send_recv(16, 233, 100000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize32Num1e5) {
 	spsc_repetitive_send_recv(32, 233, 100000);
 }
@@ -64,19 +59,15 @@ TEST(SPSC, RepetitiveSendRecvIntSize32Num1e5) {
 TEST(SPSC, RepetitiveSendRecvIntSize8Num1e6) {
 	spsc_repetitive_send_recv(8, 233, 1000000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize16Num1e6) {
 	spsc_repetitive_send_recv(16, 233, 1000000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize32Num1e6) {
 	spsc_repetitive_send_recv(32, 233, 1000000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize64Num1e6) {
 	spsc_repetitive_send_recv(64, 233, 1000000);
 }
-
 TEST(SPSC, RepetitiveSendRecvIntSize128Num1e6) {
 	spsc_repetitive_send_recv(128, 233, 1000000);
 }
